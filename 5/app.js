1f3be0f597c1d81ddd8066c6414fea0ed4751056
@patryk.bielecki <bielecki.biz@gmail.com>
2023-09-04 21:03:11 +0200

    dodanie operatorow

--- 5/app.js
+++ 5/app.js
@@ -15,7 +15,7 @@ for (let i = 0; i < number.length; i++) {
         if (resultDisplayed === false){
             input.innerHTML += e.target.innerHTML;
         } 
-        else if (resultDisplayed === true && lastChar === '+' && lastChar === '-' && lastChar === '*' && lastChar === '/'){
+        else if (resultDisplayed === true && lastChar === '+' && lastChar === '-' && lastChar === '×' && lastChar === '÷'){
             resultDisplayed = false;
             input.innerHTML += e.target.innerHTML;
         } 
@@ -25,5 +25,25 @@ for (let i = 0; i < number.length; i++) {
             input.innerHTML += e.target.innerHTML;
         }
     });
+}
+
+for (let i = 0; i < operator.length; i++){
+
+    operator[i].addEventListener('click', function(e){
+
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
+
+        if (lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '÷'){
+            let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+            input.innerHTML = newString;
+        } else if (currentString.length == 0){
+            console.log("Podaj najpierw liczbe, nie zaczynaj od operatora :>")
+        } else {
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+
 }
 
